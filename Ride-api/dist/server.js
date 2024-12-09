96588b73c156e8b2179072faeaa4554d94ab9f1e
Sowparna Shome <soupornoshome@gmail.com>
2024-12-09 23:11:28 +0530

    server check

---
+++ Ride-api/dist/server.js
@@ -0,0 +1,6 @@
+import http from "http";
+http.createServer((req, res) => {
+    res.writeHead(200, { "content-type": "text/html; charset=UTF-8" });
+    res.write("<h1>Hello there</h1>");
+    res.end();
+}).listen(4005, () => console.log(`app running at http://localhost:4005`));
