8ae6f32449ad85a3084521eb0bc7ee1ad8790641
Sowparna Shome <soupornoshome@gmail.com>
2024-12-10 23:05:49 +0530

    nodemon check

--- Ride-api/src/server.ts
+++ Ride-api/src/server.ts
@@ -2,5 +2,6 @@ import http from "http";
 http.createServer((req, res) => {
     res.writeHead(200, { "content-type": "text/html; charset=UTF-8" })
     res.write("<h1>Hello there</h1>");
+    res.write("<h2>changed again nodemon working check</h2>")
     res.end();
 }).listen(4005, () => console.log(`app running at http://localhost:4005`));
