cdb2bf1b672ae8b8862480828a6da9c253d5fa15
shome98 <soupornoshome@gmail.com>
2024-12-23 14:16:27 +0530

    blacklist token model add

---
+++ Ride-api/src/models/blackListToken.model.ts
@@ -0,0 +1,20 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IBlackListToken extends Document{
+    token:string;
+    createdAt:Date;
+}
+const blacklistTokenSchema = new Schema<IBlackListToken>({
+    token: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        expires: 86400 // 24 hours in seconds
+    }
+});
+
+export const BlackListToken = mongoose.models.BlackListToken||mongoose.model<IBlackListToken>('BlackListToken', blacklistTokenSchema);
