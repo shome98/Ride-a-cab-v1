fc18873b63bacc75dd06045b434bfb8230ec795c
shome98 <soupornoshome@gmail.com>
2024-12-24 16:04:35 +0530

    fetch distance and time to reach

--- Ride-api/src/controllers/map.controller.ts
+++ Ride-api/src/controllers/map.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { getAddressCoordinates } from "../services/map.service";
+import { getAddressCoordinates, getDistanceTime } from "../services/map.service";
 import { ApiResponse } from "../helpers/ApiResponse";
 import { ApiError } from "../helpers/ApiError";
 
@@ -16,3 +16,17 @@ export const getCoordinates=async(req:Request,res:Response)=>{
         res.status(404).json(new ApiError(404,"No coodinates were found for the address."));
     }
 }
+
+export const fetchDistanceTime=async(req:Request,res:Response)=>{
+    // const errors = validationResult(req);
+    // if (!errors.isEmpty()) {
+    //     return res.status(400).json({ errors: errors.array() });
+    // }
+    const {origin,destination}=await req.query;
+    try {
+        const distanceTime=await getDistanceTime(origin as string,destination as string);
+        res.status(200).json(new ApiResponse(200,distanceTime));
+    } catch (error) {
+        res.status(404).json(new ApiError(404,"No vales of distance and time were found for the origin and destination"));
+    }
+};
