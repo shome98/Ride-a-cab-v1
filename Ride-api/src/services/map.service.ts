ed249076e301b2b286fa1c63144dda810d3ab408
shome98 <soupornoshome@gmail.com>
2024-12-24 15:22:46 +0530

    get distance and time

--- Ride-api/src/services/map.service.ts
+++ Ride-api/src/services/map.service.ts
@@ -17,4 +17,26 @@ export const getAddressCoordinates=async(address:string)=>{
         console.error(error);
         throw error;
     }
+};
+
+export const getDistanceTime = async (origin:string, destination:string) => {
+    if (!origin || !destination) {
+        throw new Error('Origin and destination are required');
+    }
+    const apiKey = process.env.GOOGLE_MAPS_API;
+    const url = `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${encodeURIComponent(origin)}&destinations=${encodeURIComponent(destination)}&key=${apiKey}`;
+    try {
+        const response = await axios.get(url);
+        if (response.data.status === 'OK') {
+            if (response.data.rows[ 0 ].elements[ 0 ].status === 'ZERO_RESULTS') {
+                throw new Error('No routes found');
+            }
+            return response.data.rows[ 0 ].elements[ 0 ];
+        } else {
+            throw new Error('Unable to fetch distance and time');
+        }
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
 }
