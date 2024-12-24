477a876922244cad3dd52fd7b930ac4a108a51a1
Sowparna Shome <soupornoshome@gmail.com>
2024-12-24 23:13:10 +0530

    server create and other adds

--- Ride-api/src/app.ts
+++ Ride-api/src/app.ts
@@ -1,10 +1,3 @@
-// import http from "http";
-// http.createServer((req, res) => {
-//     res.writeHead(200, { "content-type": "text/html; charset=UTF-8" })
-//     res.write("<h1>Hello there</h1>");
-//     res.write("<h2>changed again nodemon working check</h2>")
-//     res.end();
-// }).listen(4005, () => console.log(`app running at http://localhost:4005`));
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
@@ -18,8 +11,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
-app.use("/api/v1/users", userRoutes);
 
+app.use("/api/v1/users", userRoutes);
 dbConnect()
     .then(() => app.listen(process.env.PORT, () => console.log(`Server is running at http://localhost:${process.env.PORT}`)))
     .catch(error => console.log(`MONGODB CONNECTION FAILED!!!. `, error));
