f69c6157afd02a153ac08ae9b7aea0a3cc14c752
Sowparna Shome <soupornoshome@gmail.com>
2025-01-05 22:34:14 +0530

    map routes register and / add with listen remove

--- Ride-api/src/app.ts
+++ Ride-api/src/app.ts
@@ -1,10 +1,11 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dbConnect from "./db/dbConnection";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.routes";
 import captainRoutes from "./routes/captain.routes";
+import mapRoutes from "./routes/map.routes";
 
 const app = express();
 dotenv.config();
@@ -13,8 +14,15 @@ app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
+app.get("/", (req: Request, res: Response) => {
+    res.send("Welcome to api!!!");
+});
+
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/captains", captainRoutes);
+app.use("/api/v1/maps", mapRoutes);
 dbConnect()
-    .then(() => app.listen(process.env.PORT, () => console.log(`Server is running at http://localhost:${process.env.PORT}`)))
+    .then(()=>console.log("connected"))
     .catch(error => console.log(`MONGODB CONNECTION FAILED!!!. `, error));
+
+export default app;
