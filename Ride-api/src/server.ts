7607e3b3dd90c46ddce20fa89b159fc6a2ff32c4
Sowparna Shome <soupornoshome@gmail.com>
2025-01-05 22:34:44 +0530

    socket use

--- Ride-api/src/server.ts
+++ Ride-api/src/server.ts
@@ -5,23 +5,31 @@
 //     res.write("<h2>changed again nodemon working check</h2>")
 //     res.end();
 // }).listen(4005, () => console.log(`app running at http://localhost:4005`));
-import express from "express";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import dbConnect from "./db/dbConnection";
-import dotenv from "dotenv";
-import userRoutes from "./routes/user.routes";
-import captainRoutes from "./routes/captain.routes";
+// import express from "express";
+// import cookieParser from "cookie-parser";
+// import cors from "cors";
+// import dbConnect from "./db/dbConnection";
+// import dotenv from "dotenv";
+// import userRoutes from "./routes/user.routes";
+// import captainRoutes from "./routes/captain.routes";
 
-const app = express();
-dotenv.config();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cors());
-app.use(cookieParser());
-app.use("/api/v1/users", userRoutes);
-app.use("/api/v1/captains", captainRoutes);
+// const app = express();
+// dotenv.config();
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(cors());
+// app.use(cookieParser());
+// app.use("/api/v1/users", userRoutes);
+// app.use("/api/v1/captains", captainRoutes);
 
-dbConnect()
-    .then(() => app.listen(process.env.PORT, () => console.log(`Server is running at http://localhost:${process.env.PORT}`)))
-    .catch(error => console.log(`MONGODB CONNECTION FAILED!!!. `, error));
+// dbConnect()
+//     .then(() => app.listen(process.env.PORT, () => console.log(`Server is running at http://localhost:${process.env.PORT}`)))
+//     .catch(error => console.log(`MONGODB CONNECTION FAILED!!!. `, error));
+
+import http from "http";
+import app from "./app";
+import { initializeSocket } from "./socket";
+import { Console } from "console";
+const server = http.createServer(app);
+initializeSocket(server);
+server.listen(process.env.PORT, () => console.log(`Server is running at http://localhost:${process.env.PORT}`));
