7f702cf92bafcd3b62be93bebabb45c0618db18a
shome98 <soupornoshome@gmail.com>
2025-01-07 15:47:07 +0530

    calculate the fare

---
+++ Ride-api/src/services/ride.service.ts
@@ -0,0 +1,18 @@
+import { getDistanceTime } from "./map.service";
+
+export async function calculateFare(pickup:string,destination:string){
+    if(!pickup || !destination) throw new Error("Pick and destination both are required ");
+    const distanceTime=await getDistanceTime(pickup,destination);
+    const baseFare={auto:30,car:50,moto:20};
+    const perKMRate={auto:10,car:15,moto:8};
+    const perMinuteRate={auto:2,car:3,moto:1.5};
+    if(distanceTime){
+        const fare={
+            auto: Math.round(baseFare.auto + ((distanceTime.distance.value / 1000) * perKMRate.auto) + ((distanceTime.duration.value / 60) * perMinuteRate.auto)),
+            car: Math.round(baseFare.car + ((distanceTime.distance.value / 1000) * perKMRate.car) + ((distanceTime.duration.value / 60) * perMinuteRate.car)),
+            moto: Math.round(baseFare.moto + ((distanceTime.distance.value / 1000) * perKMRate.moto) + ((distanceTime.duration.value / 60) * perMinuteRate.moto))
+        };
+        return fare;
+    }
+    return {};
+}
