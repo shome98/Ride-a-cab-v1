f0cff92df47a608742d80b3642896261b31324dc
shome98 <soupornoshome@gmail.com>
2025-01-13 17:08:55 +0530

    add image later

--- Ride-a-cab-web/src/pages/Home.tsx
+++ Ride-a-cab-web/src/pages/Home.tsx
@@ -5,8 +5,12 @@ export default function Home():JSX.Element{
   return (
   <div>
       <div className="h-screen w-full flex justify-between flex-col bg-slate-500">
+        <div>image goes here</div>
+        <div>backgrund image with vehicles goes here</div>
+        <div className="bg-slate-400 flex justify-center flex-col items-center">
         <h2 className="text-[30px] font-seibold">Get started with Ridee</h2>
         <button className="p-3 bg-black text-white rounded-lg m-5" onClick={()=>navigate("/sign-up")}>Continue</button>
+        </div>
       </div>
   </div>
   );
