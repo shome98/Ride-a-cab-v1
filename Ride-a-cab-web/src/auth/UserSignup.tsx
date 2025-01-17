ceec60154dd6269104f2f08741e6fef5d2318dd6
shome98 <soupornoshome@gmail.com>
2025-01-17 14:41:18 +0530

    user signup without post

--- Ride-a-cab-web/src/auth/UserSignup.tsx
+++ Ride-a-cab-web/src/auth/UserSignup.tsx
@@ -4,6 +4,8 @@ import { useNavigate } from "react-router-dom";
 export default function UserSignUp(){
     const navigate=useNavigate();
     const [email,setEmail]=useState("");
+    const [firstName,setFirstName]=useState("");
+    const [lastName,setLastName]=useState("");
     const [password,setPassword]=useState("");
     const submitHandler=(event:React.FormEvent)=>{
         event.preventDefault();
@@ -14,7 +16,26 @@ export default function UserSignUp(){
     <div>
         <h1>paste a background image here</h1>
         <form action="" method="post" onSubmit={submitHandler}>
-            <h3 className="text-lg mb-2 text-center font-medium">What's your email??</h3>
+        <h3 className="text-lg mb-2 text-center font-medium">What's your name?</h3>
+            <div className="flex gap-4 mb-2">
+            <input 
+            type="text" 
+            className="bg-slate-100 rounded px-2 py-2 text-lg border  w-full placeholder:text-m text-center" 
+            placeholder="First Name"
+            required
+            value={firstName}
+            onChange={(e)=>setFirstName(e.target.value)}
+            />
+            <input 
+            type="text" 
+            className="bg-slate-100 rounded px-2 py-2 text-lg border  w-full placeholder:text-m text-center" 
+            placeholder="Last Name"
+            required
+            value={lastName}
+            onChange={(e)=>setLastName(e.target.value)}
+            />
+            </div>
+            <h3 className="text-lg mb-2 text-center font-medium">What's your email?</h3>
             <input 
             type="email" 
             className="bg-slate-100 rounded px-2 py-2 text-lg border  w-full placeholder:text-m text-center" 
