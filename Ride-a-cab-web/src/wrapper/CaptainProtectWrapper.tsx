2ddfb852b0dbf707e0642e100c0a38a32bba7c7f
Sowparna Shome <soupornoshome@gmail.com>
2025-03-07 20:10:23 +0530

    user captain wrapper

---
+++ Ride-a-cab-web/src/wrapper/CaptainProtectWrapper.tsx
@@ -0,0 +1,47 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { CaptainDataContext } from '../context/CaptainContext';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+interface CaptainProtectWrapperProps {
+  children: React.ReactNode;
+}
+
+const CaptainProtectWrapper: React.FC<CaptainProtectWrapperProps> = ({ children }) => {
+  const token = localStorage.getItem('token');
+  const navigate = useNavigate();
+  const {  setCaptain } = useContext(CaptainDataContext);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (!token) {
+      navigate('/captain-login');
+    }
+
+    axios
+      .get(`${import.meta.env.VITE_BASE_URL}/captains/profile`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          setCaptain(response.data.captain);
+          setIsLoading(false);
+        }
+      })
+        .catch((err) => {
+            console.error(err);
+        localStorage.removeItem('token');
+        navigate('/captain-login');
+      });
+  }, [token, navigate, setCaptain]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return <>{children}</>;
+};
+
+export default CaptainProtectWrapper;
---
+++ Ride-a-cab-web/src/wrapper/UserProtectWrapper.tsx
@@ -0,0 +1,47 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { UserDataContext } from '../context/UserContext';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+interface UserProtectWrapperProps {
+  children: React.ReactNode;
+}
+
+const UserProtectWrapper: React.FC<UserProtectWrapperProps> = ({ children }) => {
+  const token = localStorage.getItem('token');
+  const navigate = useNavigate();
+  const { setUser } = useContext(UserDataContext);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (!token) {
+      navigate('/login');
+    }
+
+    axios
+      .get(`${import.meta.env.VITE_BASE_URL}/users/profile`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          setUser(response.data);
+          setIsLoading(false);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        localStorage.removeItem('token');
+        navigate('/login');
+      });
+  }, [token, navigate, setUser]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return <>{children}</>;
+};
+
+export default UserProtectWrapper;
